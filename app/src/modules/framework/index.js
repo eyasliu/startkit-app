10bdf841b107bf6cb89e85e9d71a66e1ebbbead3
Eyas <liuyuesongde@163.com>
2015-11-14 14:25:37 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- app/src/modules/framework/index.js
+++ app/src/modules/framework/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'common/style'
 import APPModule from './components/moduleContainer'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import createStore from 'utils/store'
 import reducers from './reducers'
 import Devtool from 'common/components/devtool'
@@ -9,19 +9,19 @@ import Devtool from 'common/components/devtool'
 console.log(config)
 let store = createStore()(reducers)
 
-export default class FrameWork extends React.Component{
-  constructor(){
+export default class FrameWork extends React.Component {
+  constructor() {
     super()
   }
-  render(){
-    return(
+  render() {
+    return (
       <div className="framework">
         <Provider store={store}>
           <APPModule children={this.props.children} />
         </Provider>
-        {config.debug&&config.devtool&&<Devtool store={store} />}
+        {config.debug && config.devtool && <Devtool store={store} />}
       </div>
-    )
+      )
   }
 
 }
