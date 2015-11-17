ecbcf8b58bcdb8810fb6712e8f9520955f1c48bd
Eyas <liuyuesongde@163.com>
2015-11-17 23:46:39 +0800

    Signed-off-by: Eyas <liuyuesongde@163.com>

--- webpack.dev.js
+++ webpack.dev.js
@@ -20,9 +20,9 @@ module.exports = {
   module: {
     loaders: [
       ...common.module.loaders,
-      {
+      { 
         test: /\.(js|jsx)$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['react-hot', 'babel?cacheDirectory=true'],
         include: [path.join(__dirname, 'app/src'), path.join(__dirname, './config')],
         exclude: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'app/src/vendor')]
       }
