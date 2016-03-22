bad5c7b37688b3a47550c2ccaa0dea62f40fa074
Eyas <liuyuesongde@163.com>
2016-03-22 22:40:04 +0800

    升级包，删除不必要包

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,33 +1,21 @@
 import gulp from 'gulp';
 import util from 'gulp-util';
-import childProcess from 'child_process';
+import eslint from 'gulp-eslint';
 import del from 'del';
 import webpack from 'webpack';
-import gulpWebpack from 'gulp-webpack';
-// import eslint from 'gulp-eslint';
-// import plumber from 'gulp-plumber';
-import notifier from 'node-notifier';
-import path from 'path';
 import run from 'run-sequence';
-import colors from 'colors';
 import WebpackDevServer from 'webpack-dev-server';
 import webpackDevConfig from './config/webpack.dev.js';
 import webpackProConfig from './config/webpack.pro.js';
 import config from './config/config';
 
-const $ = require('gulp-load-plugins')();
 
 // dev server
 gulp.task('dev', ()=>{
   const compiler = webpack(webpackDevConfig);
 
   compiler.plugin('done', (stats) => {
-    const statsStr = stats.toString();
-    console.log(statsStr.substr(0, statsStr.indexOf('[rendered]')).green);
     run('lint');
-  });
-  compiler.plugin('failed', (err) => {
-    console.log(err.red);
   });
 
   new WebpackDevServer( compiler, {
@@ -39,7 +27,8 @@ gulp.task('dev', ()=>{
     noInfo: false,
     inline: true,
     stats: {
-      colors: true
+      colors: true,
+      chunks: false
     }
   }).listen(config.clientPort, config.host, (err, stats)=>{
     if (err) util.log(err);
@@ -49,7 +38,7 @@ gulp.task('dev', ()=>{
 
 gulp.task('lint', () => {
   return gulp.src(['./*.js', 'app/src/**/*.js', 'app/src/**/*.jsx', '!app/src/vendor/**/*'])
-    .pipe($.eslint({
+    .pipe(eslint({
       globals: {
         'React': true,
         '$': true,
@@ -59,30 +48,18 @@ gulp.task('lint', () => {
         'config': true
       }
     }))
-    .pipe($.plumber({
-      errorHandler(err) {
-        const { fileName, lineNumber, message } = err;
-        const relativeFilename = path.relative(process.cwd(), fileName);
-        notifier.notify({
-          title: 'ESLint Error',
-          wait: true,
-          message: `Line ${lineNumber}: ${message} (${relativeFilename})`
-        }, (error, msg) => {
-          if (msg.startsWith('Activate')) {
-            childProcess.exec(`subl --command open_file ${fileName}:${lineNumber}`);
-          }
-        });
-      }
-    }))
-    .pipe($.eslint.failOnError())
-    .pipe($.eslint.formatEach());
+    .pipe(eslint.failOnError())
+    .pipe(eslint.formatEach());
 });
 
 // build
 gulp.task('build', ['clean'], ()=>{
-  gulp.src('src/app.js')
-    .pipe(gulpWebpack(webpackProConfig))
-    .pipe(gulp.dest('./build'));
+  webpack(webpackDevConfig, (err, stats) => {
+    console.log(stats.toString({
+      chunks: false,
+      colors: true
+    }))
+  })
 });
 
 // build on save
